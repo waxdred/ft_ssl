02b79c0348a079da44ecea77b11abfe5998e4135
jo <waxcoin@hotmail.com>
2024-05-14 10:31:29 +0200

    feat: Write data from buf using Write callback

---
+++ srcs/readline.c
@@ -0,0 +1,16 @@
+#include "../includes/utils.h"
+
+void ft_readline(char *file, void (*Write)(unsigned char *f)) {
+  int fd;
+  char buf[512];
+  int ret;
+  ft_bzero(buf, 512);
+  if ((fd = OpenFile(file)) != -1) {
+    while ((ret = read(fd, buf, 512)) > 0) {
+      buf[ret] = '\0';
+      Write((unsigned char *)(buf));
+      ft_bzero(buf, 512);
+    }
+  }
+  close(fd);
+}
