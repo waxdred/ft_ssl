08179a7212cdfc75c3b8644f8ceace72ef4bbc92
jo <waxcoin@hotmail.com>
2024-05-14 13:09:08 +0200

    feat: Introduce ft_printf for formatted output

---
+++ includes/ft_printf.h
@@ -0,0 +1,11 @@
+#ifndef FT_PRINTF_H
+#define FT_PRINTF_H
+#include "utils.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void ft_printf(const char *format, ...);
+
+#endif
